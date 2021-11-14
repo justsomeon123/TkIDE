4a28cd8018d23544a9a41a4fa73a39fc9175ec02
justsomeon123 <code80191@gmail.com>
2021-11-14 16:51:28 -0500

    Squashed commit of the following:
    commit fe57662e284070b070f26e8b365c1ff2721e4d57
    Author: justsomeon123 <code80191@gmail.com>
    Date:   Sun Nov 14 16:42:56 2021 -0500
    
        Update .gitignore
    
    commit c9fc26a128a689b847a376d1bf4480bcc1bcb543
    Author: justsomeon123 <code80191@gmail.com>
    Date:   Sun Nov 14 16:40:19 2021 -0500
    
        not important
    
    commit 3c18d2ae221e71c0f90ff24e975dd4be5f693e9f
    Author: justsomeon123 <code80191@gmail.com>
    Date:   Sun Nov 14 16:39:35 2021 -0500
    
        sry forgot to do stuff
    
    commit 2b30032634b4f21a3d0e2a0c41d6bd7b81b383af
    Author: justsomeon123 <code80191@gmail.com>
    Date:   Sun Nov 14 16:38:13 2021 -0500
    
        Added extension running basis. Changed some file names to be more accurate.

--- extensions/__init__.py
+++ extensions/__init__.py
@@ -1,10 +1,32 @@
 from os.path import dirname, basename, isfile, join
 import glob,json
+def LoadExtensionPt(sections:list):
+    if len(sections) == 0:
+        return
+    if len(sections) == 1:
+        section = sections[0]
+        exec("exec(commands[section])")
+    if len(sections) == 2:
+        section = sections[0]
+        subsection = sections[1]
+        exec("exec(commands[section][subsection])")
+    if len(sections) == 3:
+        section = sections[0]
+        subsection = sections[1]
+        subsection1 = sections[2]
+        exec("exec(commands[section][subsection][subsection1])")
+    if len(sections) == 4:
+        section = sections[0]
+        subsection = sections[1]
+        subsection1 = sections[2]
+        subsection2 = sections[3]
+        exec("exec(commands[section][subsection][subsection1][subsection2])")
+    else:return
+    
 modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
 print(__all__)
-bootstrap = json.load(open(glob.glob(join(dirname(__file__), "bootstrap.json"))[0]))
-print(bootstrap)
-def LoadExtensionPt(section,subsection):
-    exec("exec(bootstrap[section][subsection])")
+commands = json.load(open("extensions/commands.json"))
+print(commands)
+
 
--- test.py
+++ test.py
@@ -2,5 +2,3 @@ import time
 for i in range(10000):
     print(i+1)
     time.sleep(0.1)
-
-apple = 2312
